282615f0a236e29ebac7583efe7cd9bf8c03b656
Gregory Williams <grw300@gmail.com>
2021-07-11 15:03:04 -0700

    Add backpropogation to the ReLU activation func

--- activation.py
+++ activation.py
@@ -3,7 +3,14 @@ import numpy as np
 class ReLUActivation:
 
     def forward(self, inputs):
+        self.inputs = inputs
         self.output = np.maximum(0, inputs)
+    
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+
+        self.dinputs[self.inputs <= 0] = 0
+
 
 class SoftmaxActivation:
     
