1cd9098220215a3f651f3dc465fdf554b8a41bfd
Gregory Williams <grw300@gmail.com>
2021-07-25 13:23:57 -0700

    Add backwards for softmax activation

--- activation.py
+++ activation.py
@@ -20,3 +20,12 @@ class SoftmaxActivation:
         probabilites = exp_values / np.sum(exp_values, axis=1, keepdims=True)
 
         self.output = probabilites
+    
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+        
+        for idx, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
+            single_output = single_output.reshape(-1, 1)
+            jacobian = np.diagflat(single_output) - np.dot(single_output, single_output.T)
+
+            self.dinputs[idx] = np.dot(jacobian, single_dvalues)
